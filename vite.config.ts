de275326f6e63b8d16ed4075bfb2bf1ea816317d
mejat9806 <110531287+mejat9806@users.noreply.github.com>
2026-03-29 13:53:43 +0800

    Refactor vite.config.ts: add nitro configuration for production mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
 import { defineConfig } from 'vite';
 import { devtools } from '@tanstack/devtools-vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { nitro } from 'nitro/vite';
+
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  server: {},
   plugins: [
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     tanstackStart(),
+    mode === 'production'
+      ? nitro({
+          routeRules: {
+            '/dist/client/assets': {
+              headers: {
+                'cache-control': 'public, max-age=31536000, immutable',
+              },
+            },
+          },
+        })
+      : null,
     viteReact(),
   ],
-});
+}));
